4f3d698c938a1ebf16fd52e88cda62faaa6852bb
Jeremy Kent <jkent@jkent-vb-bunt.(none)>
2013-02-28 11:04:55 -0800

    removed inapplicable code

--- cgi-bin/admin.py
+++ cgi-bin/admin.py
@@ -24,9 +24,6 @@ function updateInitiative() {
   var xmlhttp;
   if (window.XMLHttpRequest) { xmlhttp=new XMLHttpRequest(); }
   else { xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); }
-  xmlhttp.onreadystatechange=function() {
-      if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-        document.getElementById("myDiv").innerHTML=xmlhttp.responseText; } }
   xmlhttp.open("POST","update.py", false);
   xmlhttp.send(); 
   window.location.reload();
