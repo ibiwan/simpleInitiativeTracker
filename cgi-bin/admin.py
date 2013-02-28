aaaa17ca329d91496f2e0aaa8446933e3afbedb2
Jeremy "Ibi-Wan" Kent "...obi" <ibiwan@gmail.com>
2013-02-27 17:49:41 -0800

    compression and cleanup

--- cgi-bin/admin.py
+++ cgi-bin/admin.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python
 rawlist = []; beforecur = []; aftercur = []
-
-curf = open('../current_initiative.txt')
-cur = float(curf.read().strip())
-curf.close()
-
-initf = open('../initiatives.csv')
-inits = initf.readlines()
-initf.close()
+with open('../current_initiative.txt') as curf:
+  cur = float(curf.read().strip())
+with open('../initiatives.csv') as initf:
+  inits = initf.readlines()
 
 for initrow in inits:
   fields = initrow.split(',')
   fields = [x.strip() for x in fields]
-  rawlist.append((float(fields[0]), int(float(fields[0])), fields[1], fields[2]))
+  rawlist.append((float(fields[0]), int(float(fields[0])), fields[1], fields[2])) # (27.6, 27, kahlan, gm details)
 rawlist.sort(reverse=True)
 
 for row in rawlist:
-  if row[0] > cur: beforecur.append(row)
-  else:            aftercur.append(row)
+  (beforecur if row[0] > cur else aftercur).append(row)
 displist = aftercur + beforecur
 
-content = "Content-Type: text/html\n\n"
+content    = "Content-Type: text/html\n\n"
 headeropen = """<!DOCTYPE html><html><head><meta http-equiv="refresh" content="5" >"""
-admin = """<script>
+admin      = """<script>
 function updateInitiative() {
   var xmlhttp;
   // code for IE7+, Firefox, Chrome, Opera, Safari
@@ -38,13 +33,11 @@ function updateInitiative() {
   //history.go(0);
 }
 </script></head><body><button type="button" onclick="updateInitiative()">Update Initiative</button><table>"""
-rowtext = "<tr><td align=\"right\">%s</td><td> </td><td>%s</td><td>%s</td></tr>"
+rowtext    = "<tr><td align=\"right\">%s</td><td> </td><td>%s</td><td>%s</td></tr>"
 closetable = "</table>"
-footer = "</body></html>"
+footer     = "</body></html>"
 
 print content + headeropen + admin
 for row in displist:
   print rowtext % (row[1], row[2], row[3])
-print closetable 
-#print " " + str(cur) + " : " + str(beforecur) + " : " + str(aftercur) + " " + 
-print footer
+print closetable + footer
