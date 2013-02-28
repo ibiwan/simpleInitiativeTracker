8018ad8adff909e468a2e9a73f4af019148b9605
Jeremy Kent <jkent@jkent-vb-bunt.(none)>
2013-02-28 11:17:44 -0800

    changed POST call headers

--- cgi-bin/admin.py
+++ cgi-bin/admin.py
@@ -24,6 +24,7 @@ function updateInitiative() {
   var xmlhttp;
   if (window.XMLHttpRequest) { xmlhttp=new XMLHttpRequest(); }
   else { xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); }
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xmlhttp.onreadystatechange=function() { 
     if (xmlhttp.readyState==4 && xmlhttp.status==200) 
       window.location.reload; }
