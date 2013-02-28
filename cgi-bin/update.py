c46d13af6fb5cd2a88bdfa84eb59e891ef838a74
Jeremy "Ibi-Wan" Kent "...obi" <ibiwan@gmail.com>
2013-02-27 17:56:26 -0800

    close files!

--- cgi-bin/update.py
+++ cgi-bin/update.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python
-curf = open('../current_initiative.txt', 'r+')
-cur = float(curf.read().strip())
-
-#inits = []
-#initrows = open('../initiatives.csv').readlines()
-#for initrow in initrows:
-#  inits.append(float(initrow.split(',')[0].strip()))
-inits = [float(initrow.split(',')[0].strip()) for initrow in open('../initiatives.csv').readlines()]
-
-next = max([x for x in inits if x < cur] or [max(inits or [0.0])])
+with open('../current_initiative.txt', 'r+') as curf:
+  cur = float(curf.read().strip())
   
-curf.seek(0)
-curf.write(str(next))
-curf.close()
-
-print "Content-Type: text/html\n\n"
+  with open('../initiatives.csv') as initf:
+    inits = [float(initrow.split(',')[0].strip()) for initrow in initf.readlines()]
+  
+  next = max([x for x in inits if x < cur] or [max(inits or [0.0])])
+    
+  curf.seek(0)
+  curf.write(str(next))
