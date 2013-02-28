cbf65ba40b4f9d843c00b4e171724ae3831bced6
Jeremy "Ibi-Wan" Kent "...obi" <ibiwan@gmail.com>
2013-02-27 19:39:28 -0800

    Update cgi-bin/inits.py

--- cgi-bin/inits.py
+++ cgi-bin/inits.py
@@ -16,7 +16,7 @@ displist = aftercur + beforecur
 
 content    = "Content-Type: text/html\n\n"
 headeropen = """<!DOCTYPE html><html><head><meta http-equiv="refresh" content="5" >"""
-nocache    = """<META HTTP-EQUIV="Pragma" CONTENT="no-cache"> <META HTTP-EQUIV="Expires" CONTENT="-1">"""
+nocache    = """<meta name="viewport" content="width = 320" /><META HTTP-EQUIV="Pragma" CONTENT="no-cache"> <META HTTP-EQUIV="Expires" CONTENT="-1">"""
 bodytable  = "</head><body><table>"
 rowtext    = "<tr><td align=\"right\">%s</td><td> </td><td>%s</td></tr>"
 footer     = "</table></body>%s</html>"
