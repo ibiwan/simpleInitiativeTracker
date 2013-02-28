87ed6e44cb17275ddcba268317ad615fbccf2616
Jeremy "Ibi-Wan" Kent "...obi" <ibiwan@gmail.com>
2013-02-27 20:11:59 -0800

    Update cgi-bin/update.py

--- cgi-bin/update.py
+++ cgi-bin/update.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-with open('../current_initiative.txt', 'r+') as curf:
-  cur = float(curf.read().strip())
+curf = open('../current_initiative.txt', 'r+')
+cur = float(curf.read().strip())
   
-  with open('../initiatives.csv') as initf:
-    inits = [float(initrow.split(',')[0].strip()) for initrow in initf.readlines()]
+with open('../initiatives.csv') as initf:
+  inits = [float(initrow.split(',')[0].strip()) for initrow in initf.readlines()]
   
-  next = max([x for x in inits if x < cur] or [max(inits or [0.0])])
+next = max([x for x in inits if x < cur] or [max(inits or [0.0])])
     
-  curf.seek(0)
-  curf.write(str(next))
+curf.seek(0)
+curf.write(str(next))
