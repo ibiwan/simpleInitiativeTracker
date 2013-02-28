d9e721c48197d73f8ac5f9b692caebe76593e6a8
Jeremy "Ibi-Wan" Kent "...obi" <ibiwan@gmail.com>
2013-02-27 17:34:13 -0800

    simplify logic (needs test)

--- cgi-bin/update.py
+++ cgi-bin/update.py
@@ -5,9 +5,7 @@ cur = float(curf.read().strip())
 inits = []
 initrows = open('../initiatives.csv').readlines()
 for initrow in initrows:
-  fields = initrow.split(',')
-  init = fields[0].strip()
-  inits.append(float(init))
+  inits.append(float(initrow.split(',')[0].strip()))
 
 next = max([x for x in inits if x < cur] or [max(inits or [0.0])])
   
