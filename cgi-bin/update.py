31abcc9f756b8f74c6aa6fd7e8efd1bd693e4c51
Jeremy Kent <jkent@jkent-vb-bunt.(none)>
2013-02-28 10:41:31 -0800

    fixed old-python buggery

--- cgi-bin/update.py
+++ cgi-bin/update.py
@@ -2,8 +2,8 @@
 curf = open('../current_initiative.txt', 'r+')
 cur = float(curf.read().strip())
   
-with open('../initiatives.csv') as initf:
-  inits = [float(initrow.split(',')[0].strip()) for initrow in initf.readlines()]
+initf = open('../initiatives.csv')
+inits = [float(initrow.split(',')[0].strip()) for initrow in initf.readlines()]
   
 next = max([x for x in inits if x < cur] or [max(inits or [0.0])])
     
